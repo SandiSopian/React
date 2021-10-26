62d96d017677e5236e4a3a10ecefc432499c3623
Sandi Sopian <shunasand73@gmail.com>
2021-10-27 05:28:31 +0700

    install component

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+          <i class="fas fa-cart-plus"></i>
+          </div>
+          <div class="col-sm">
+            Column
+          </div>
+          <div class="col-sm">
+            Column
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
